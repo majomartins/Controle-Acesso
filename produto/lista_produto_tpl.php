56e15211b1ad540a7836a66b28562f389383e7bb
JoanSants <joansants07@gmail.com>
2017-05-18 17:48:22 -0300

    atualizado

--- produto/lista_produto_tpl.php
+++ produto/lista_produto_tpl.php
@@ -4,34 +4,15 @@ include "../menu/index.head.tpl.php";
 include "../menu/index.body.tpl.php";
 
 ?>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
 <body>
-	<div class="container">
-	<table class="table">
-	<thead>
-		<tr>
-			<th>Produto</th>
-			<th>Nome</th>
-			<th>Descrição</th>
-			<th>Preço</th>
-			<th>Promoção</th>
-			<th>Categoria</th>
-			<th>Ativo</th>
-			<th>Usuario</th>
-			<th>Estoque</th>
-			<th>Imagem</th>
-			<th>
+	<center>
+	<table class="table" width="90%">
 				<?php
 					if($_SESSION['tipoPerfil'] == "A"){
 				
 						echo '<a href="?acao=incluir">';
 				
-						echo"<font colspan ='2' color='green'>
+						echo"<font color='green'>
 						+ Novo Produto
 						</font>
 						</a>";
@@ -39,9 +20,6 @@ include "../menu/index.body.tpl.php";
 						echo "Novo Produto";
 					}
 				?>
-			<th>
-		<tr>
-		</thead>
 		<?php
 		
 		
@@ -65,25 +43,47 @@ include "../menu/index.body.tpl.php";
 		}else{
 			foreach($produtos as $produto){
 								
-				echo "	<tr>
-							<td>".utf8_encode($produto['idProduto'])."</td>
-							<td>".utf8_encode($produto['nomeProduto'])."</td>
-							<td>".utf8_encode($produto['descProduto'])."</td>
-							<td>".utf8_encode($produto['precProduto'])."</td>
-							<td>".utf8_encode($produto['descontoPromocao'])."</td>
-							<td>".utf8_encode($produto['idCategoria'])."</td>
-							<td>".utf8_encode($produto['ativoProduto'])."</td>
-							<td>".utf8_encode($produto['idUsuario'])."</td>
-							<td>".utf8_encode($produto['qtdMinEstoque'])."</td>
-							<td><img width = '100px' src='data:image/jpeg;base64,".base64_encode($produto['imagem'])."' /></td>	
-							<td><a href='?acao=editar&id={$produto['idProduto']}'>Editar</a></td>
-							<td><a href='?acao=excluir&id={$produto['idProduto']}'>Excluir</a></td>
+				echo "  
+						
+						<tr>
+							<td width='10%'><label class='cabecalhos'>ID Produto:</label>".utf8_encode($produto['idProduto'])."</td>
+							<td rowspan='5' width='30%'>".utf8_encode($produto['descProduto'])."</td>
+							<td rowspan ='5' width='10%'><img width='150px'src='data:image/jpeg;base64,".base64_encode($produto['imagem'])."' /></td>
+							<td width='10%'><label class='cabecalhos'>Preço:</label>".utf8_encode($produto['precProduto'])."</td>
+							<td rowspan ='2' width='5%'><a href='?acao=editar&id={$produto['idProduto']}'>Editar</a></td>
+						</tr>
+						
+						<tr>
+							<td width='15%'>".utf8_encode($produto['nomeProduto'])."</td>
+							<td width='10%'><label class='cabecalhos'>Desconto: </label>".utf8_encode($produto['descontoPromocao'])."</td>
+							
+							
+						</tr>
+						
+						<tr>
+						
+						<tr>
+							<td><label class='cabecalhos'>Ativo:</label>".utf8_encode($produto['ativoProduto'])."</td>
+							<td width='10%'><label class='cabecalhos'>Categoria:</label>".utf8_encode($produto['idCategoria'])."</td>
+							<td rowspan='2' width='5%'><a href='?acao=excluir&id={$produto['idProduto']}'>Excluir</a></td>
+						</tr>
+						
+						<tr>	
+							<td><label class='cabecalhos'>Usuário:</label>".utf8_encode($produto['idUsuario'])."</td>
+							<td width='10%'><label class='cabecalhos'>Estoque:</label>".utf8_encode($produto['qtdMinEstoque'])."</td>
+						</tr>		
+							
+							
+						</tr>
+						<tr>
+						<div height='200px'>
+						</div>
 						</tr>";		
 			}
 		}
 		?>
 	</table>
-	</div>
+	</center>
 </body>
 
 <?php
